d1c233ca3ed19e901e7c0d4e688c2a1eedeead4d
ADCO <danieladeshina01@gmail.com>
2024-03-13 03:53:45 +0100

    DB-update

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:QWe45YU.@localhost:5432/ScissorApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:QWe45YU.@localhost:5432/ScissorApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://gvpzdzwi:4Kh0inaihxsQMl-UgQKW2xQztDNtilSj@ziggy.db.elephantsql.com/gvpzdzwi"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL)
